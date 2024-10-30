7f6a84b8372e1275b0a611f408745370d57e940e
BekLg <bereketlegesse22@gmail.com>
2024-10-30 11:22:18 +0300

    able to calculate MSE

--- Model Comparison/Latest Version/pc_discrim/train_pcn.py
+++ Model Comparison/Latest Version/pc_discrim/train_pcn.py
@@ -2,7 +2,7 @@ from jax import numpy as jnp, random
 import sys, getopt as gopt, optparse, time
 from pcn_model import PCN ## bring in model from museum
 ## bring in ngc-learn analysis tools
-from ngclearn.utils.metric_utils import measure_ACC, measure_CatNLL
+from ngclearn.utils.metric_utils import measure_ACC, measure_CatNLL, measure_MSE
 
 """
 ################################################################################
@@ -53,7 +53,7 @@ x_dim = _X.shape[1]
 patch_shape = (int(jnp.sqrt(x_dim)), int(jnp.sqrt(x_dim)))
 y_dim = _Y.shape[1]
 
-n_iter = 100
+n_iter = 5
 mb_size = 250
 n_batches = int(_X.shape[0]/mb_size)
 save_point = 5 ## save model params every modulo "save_point"
@@ -70,108 +70,81 @@ model = PCN(subkeys[1], x_dim, y_dim, hid1_dim=512, hid2_dim=512, T=20,
 model.save_to_disk() # save final state of synapses to disk
 print("--- Starting Simulation ---")
 
-def eval_model(model, Xdev, Ydev, mb_size): ## evals model's test-time inference performance
-    n_batches = int(Xdev.shape[0]/mb_size)
-
+# Define evaluation function with accuracy, NLL, and MSE logging
+def eval_model(model, Xdev, Ydev, mb_size):
+    n_batches = int(Xdev.shape[0] / mb_size)
+    nll, acc, mse = 0, 0, 0
     n_samp_seen = 0
-    nll = 0. ## negative Categorical log liklihood
-    acc = 0. ## accuracy
+
     for j in range(n_batches):
-        ## extract data block/batch
         idx = j * mb_size
-        Xb = Xdev[idx: idx + mb_size,:]
-        Yb = Ydev[idx: idx + mb_size,:]
-        ## run model inference
+        Xb = Xdev[idx: idx + mb_size, :]
+        Yb = Ydev[idx: idx + mb_size, :]
+
+        # Run model inference
         yMu_0, yMu, _ = model.process(obs=Xb, lab=Yb, adapt_synapses=False)
-        ## record metric measurements
-        _nll = measure_CatNLL(yMu_0, Yb) * Xb.shape[0] ## un-normalize score
-        _acc = measure_ACC(yMu_0, Yb) * Yb.shape[0] ## un-normalize score
-        nll += _nll
-        acc += _acc
+
+        # Record metrics
+        nll += measure_CatNLL(yMu_0, Yb) * Xb.shape[0]
+        acc += measure_ACC(yMu_0, Yb) * Yb.shape[0]
+        mse += measure_MSE(yMu_0, Yb, preserve_batch=False) * Xb.shape[0]
 
         n_samp_seen += Yb.shape[0]
 
-    nll = nll/(Xdev.shape[0]) ## calc full dev-set nll
-    acc = acc/(Xdev.shape[0]) ## calc full dev-set acc
-    return nll, acc
+    nll /= Xdev.shape[0]
+    acc /= Xdev.shape[0]
+    mse /= Xdev.shape[0]
+    return nll, acc, mse
 
-trAcc_set = []
-acc_set = []
-efe_set = []
+# Logging metrics
+trAcc_set, acc_set, efe_set, mse_set = [], [], [], []
+sim_start_time = time.time()
 
-sim_start_time = time.time() ## start time profiling
+# Initial evaluation on training and dev sets
+_, tr_acc, tr_mse = eval_model(model, _X, _Y, mb_size=1000)
+nll, acc, mse = eval_model(model, Xdev, Ydev, mb_size=1000)
 
-_, tr_acc = eval_model(model, _X, _Y, mb_size=1000)
-nll, acc = eval_model(model, Xdev, Ydev, mb_size=1000)
-print("-1: Dev: Acc = {}  NLL = {} | Tr: Acc = {} EFE = --".format(acc, nll, tr_acc))
-if verbosity >= 2:
-    print(model._get_norm_string())
-trAcc_set.append(tr_acc) ## random guessing is where models typically start
-acc_set.append(acc)
-efe_set.append(-2000.)
-jnp.save("exp/dev_acc.npy", jnp.asarray(acc_set))
-jnp.save("exp/efe.npy", jnp.asarray(efe_set))
+print(f"-1: Dev: Acc = {acc}, NLL = {nll}, MSE = {jnp.mean(mse)} | Tr: Acc = {tr_acc}, MSE = {jnp.mean(tr_mse)}")
 
+# Training loop
 for i in range(n_iter):
-    ## shuffle data (to ensure i.i.d. assumption holds)
     dkey, *subkeys = random.split(dkey, 2)
-    ptrs = random.permutation(subkeys[0],_X.shape[0])
-    X = _X[ptrs,:]
-    Y = _Y[ptrs,:]
+    ptrs = random.permutation(subkeys[0], _X.shape[0])
+    X, Y = _X[ptrs, :], _Y[ptrs, :]
 
-    ## begin a single epoch
-    n_samp_seen = 0
-    train_EFE = 0. ## training free energy (online) estimate
-    trAcc = 0. ## training accuracy score
-    for j in range(n_batches):
-        dkey, *subkeys = random.split(dkey, 2)
-        ## sample mini-batch of patterns
-        idx = j * mb_size #j % 2 # 1
-        Xb = X[idx: idx + mb_size,:]
-        Yb = Y[idx: idx + mb_size,:]
-        ## perform a step of inference/learning
+    train_EFE, n_samp_seen = 0, 0
+
+    for j in range(int(_X.shape[0] / mb_size)):
+        idx = j * mb_size
+        Xb, Yb = X[idx: idx + mb_size, :], Y[idx: idx + mb_size, :]
+        
+        # Perform training step
         yMu_0, yMu, _EFE = model.process(obs=Xb, lab=Yb, adapt_synapses=True)
-        ## track online training EFE and accuracy
         train_EFE += _EFE * mb_size
         n_samp_seen += Yb.shape[0]
-        if verbosity >= 1:
-            print("\r EFE = {} over {} samples ".format((train_EFE/n_samp_seen),
-                                                        n_samp_seen), end="")
-    if verbosity >= 1:
-        print()
 
-    ## evaluate current progress of model on dev-set
-    nll, acc = eval_model(model, Xdev, Ydev, mb_size=1000)
-    _, tr_acc = eval_model(model, _X, _Y, mb_size=1000)
-    if (i+1) % save_point == 0 or i == (n_iter-1):
-        model.save_to_disk(params_only=True) # save final state of synapses to disk
-        jnp.save("exp/trAcc.npy", jnp.asarray(trAcc_set))
-        jnp.save("exp/acc.npy", jnp.asarray(acc_set))
-        jnp.save("exp/efe.npy", jnp.asarray(efe_set))
-    ## record current generalization stats and print to I/O
+    # Periodic evaluation on dev set
+    nll, acc, mse = eval_model(model, Xdev, Ydev, mb_size=1000)
+    _, tr_acc, tr_mse = eval_model(model, _X, _Y, mb_size=1000)
+    
+    # Save and log metrics
     trAcc_set.append(tr_acc)
     acc_set.append(acc)
-    efe_set.append((train_EFE/n_samp_seen))
-    io_str = ("{} Dev: Acc = {}, NLL = {} | "
-              "Tr: Acc = {}, EFE = {}"
-             ).format(i, acc, nll, tr_acc, (train_EFE/n_samp_seen))
-    if verbosity >= 1:
-        print(io_str)
-    else:
-        print("\r{}".format(io_str), end="")
-    if verbosity >= 2:
-        print(model._get_norm_string())
-if verbosity == 0:
-    print("")
+    mse_set.append(mse)
+    efe_set.append(train_EFE / n_samp_seen)
 
-## stop time profiling
-sim_end_time = time.time()
-sim_time = sim_end_time - sim_start_time
-sim_time_hr = (sim_time/3600.0) # convert time to hours
+    MSE = jnp.array(mse_set)
+    print(f"{i}: Dev: Acc = {acc}, NLL = {nll}, MSE = {jnp.mean(MSE)} | Tr: Acc = {tr_acc}, Tr: MSE = {jnp.mean(tr_mse)}, EFE = {train_EFE / n_samp_seen}")
 
+    if (i + 1) % 5 == 0 or i == (n_iter - 1):
+        model.save_to_disk(params_only=True)
+
+# Stop time profiling
+sim_time = time.time() - sim_start_time
 print("------------------------------------")
-vAcc_best = jnp.amax(jnp.asarray(acc_set))
-print(" Trial.sim_time = {} h  ({} sec)  Best Acc = {}".format(sim_time_hr, sim_time, vAcc_best))
+print(f"Simulation Time = {sim_time / 3600.0} hrs")
+print(f"Best Dev Accuracy = {jnp.amax(jnp.asarray(acc_set))}")
+
 
 jnp.save("exp/trAcc.npy", jnp.asarray(trAcc_set))
 jnp.save("exp/acc.npy", jnp.asarray(acc_set))
