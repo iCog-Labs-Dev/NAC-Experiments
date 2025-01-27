4631c84865200a23f6d8954f82f5390bd9b1ca79
quinbez <bezawitt9@gmail.com>
2025-01-27 15:09:25 +0300

    feat: Add Decoder class to GAN_AE model with weight initialization

--- Model Comparison/Backprop/GAN_AE/gan_ae_model.py
+++ Model Comparison/Backprop/GAN_AE/gan_ae_model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -24,3 +25,25 @@ class Encoder(nn.Module):
         mu = self.fc_mu(x)
         logvar = self.fc_logvar(x)
         return mu, logvar
+    
+class Decoder(nn.Module):
+    def __init__(self, latent_dim, hidden_dim, input_dim):
+        super(Decoder, self).__init__()
+        self.fc1 = nn.Linear(latent_dim, hidden_dim[0])
+        self.fc2 = nn.Linear(hidden_dim[0], hidden_dim[1])
+        self.fc3 = nn.Linear(hidden_dim[1], input_dim)
+
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in[self.fc1, self.fc2, self.fc3]:
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0.0, std=0.055)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+
+    def forward(self, z):
+        z = F.relu(self.fc1(z))
+        z = F.relu(self.fc2(z))
+        z = torch.sigmoid(self.fc3(z))
+        return z
