ddffab52703062110a2518d038d08e66716181c5
quinbez <bezawitt9@gmail.com>
2025-01-27 15:46:21 +0300

    feat: Implement training function for GANAE model with BCE loss computation.

--- Model Comparison/Backprop/GAN_AE/gan_ae.py
+++ Model Comparison/Backprop/GAN_AE/gan_ae.py
@@ -1,6 +1,10 @@
 import logging
 import sys
 import os
+import torch
+import torch.nn.functional as F
+import numpy as np
+import tqdm
 from torch.utils.data import DataLoader
 import getopt as gopt
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -54,3 +58,26 @@ dev_loader = DataLoader(dataset=dev_dataset, batch_size=200, shuffle=False)
 
 test_dataset = NumpyDataset(testX, testY)
 test_loader = DataLoader(dataset=test_dataset, batch_size = 200, shuffle = False)
+
+def train(model, loader, optimizer, epoch):
+    model.train()
+    total_losses = []
+
+    for batch_idx, (data, _) in enumerate(tqdm(loader)):
+        data = (data > 0.5).float()
+        data = data.view(data.size(0), -1)
+        x_recon, real_or_fake, mu, logvar, l2_penalty = model(data)
+
+        reconstruction_loss = F.binary_cross_entropy(x_recon, data, reduction="sum")
+        discriminator_loss = F.binary_cross_entropy(real_or_fake, torch.ones_like(real_or_fake), reduction= "sum")
+        total_loss = reconstruction_loss + discriminator_loss + l2_penalty
+
+        optimizer.zero_grad()
+        total_loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5.0)
+        optimizer.step()
+
+        total_losses.append(total_loss.item() / data.size(0))
+
+    avg_loss = np.mean(total_losses)
+    return avg_loss
