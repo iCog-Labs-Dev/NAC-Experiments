da63878747e83baf5e86ff559e303e93da1bd6bd
quinbez <bezawitt9@gmail.com>
2025-01-28 20:41:22 +0300

    refactor: moving M-MSE and classification error functions to utils.metrics for reuse.

--- Model Comparison/Backprop/GAN_AE/gan_ae.py
+++ Model Comparison/Backprop/GAN_AE/gan_ae.py
@@ -11,10 +11,9 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader
 import getopt as gopt
 from gan_ae_model import GANAE
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.numpy_dataset import NumpyDataset
+from utils.metrics import classification_error, masked_mse
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from density.fit_gmm import fit_gmm
 from density.eval_logpx import evaluate_logpx
@@ -140,91 +139,6 @@ def evaluate(model, loader, n_components=75, num_samples = 5000):
     results['Total_inference_time'] = time.time() - inference_start_time
     logging.info(f"Total inference time: {results['Total_inference_time']:.2f} sec")
     return results
-
-def masked_mse(model, loader):
-    model.eval()
-    total_mse = 0.0
-    total_samples = 0
-    total_masked_elements = 0
-    with torch.no_grad():
-        for data, _ in loader:
-
-            data = data.view(data.size(0), -1)
-            data = (data > 0.5).float()
-            mask = torch.ones_like(data, dtype=torch.bool)
-            mask[:, : data.size(1) // 2] = 0
-
-            masked_data = data * mask.float()
-            masked_data = (masked_data > 0.5).float()
-
-            mu, _ = model.encoder(masked_data)
-            reconstructed = model.decoder(mu)  
-            reconstructed = reconstructed.view(data.size(0), -1)
-
-            mse = F.mse_loss(reconstructed[~mask], data[~mask], reduction="sum")
-            total_mse += mse.item() * data.size(0)
-            total_samples += data.size(0)
-            total_masked_elements += (~mask).sum().item()
-
-    avg_mse = total_mse / (total_samples * data.size(1) // 2)
-    return avg_mse
-
-def extract_latents(encoder, dataloader):
-    """
-    Extracts latent representations from a trained encoder.
-    
-    Parameters:
-    encoder: Trained encoder model.
-    dataloader: Dataloader containing the dataset.
-
-    Returns:
-        Extracted latent representations, and corresponding labels.
-    """
-    encoder.eval()
-    latents, labels = [], []
-
-    with torch.no_grad():
-        for batch_X, batch_Y in dataloader:
-            batch_X = batch_X 
-            batch_X = (batch_X > 0.5).float()  
-
-            output = encoder(batch_X)
-
-            if isinstance(output, tuple):  
-                Z = output[0] 
-            else:
-                Z = output 
-
-            latents.append(Z.cpu().numpy()) 
-            labels.append(batch_Y.cpu().numpy())
-
-    return np.vstack(latents), np.hstack(labels) 
-
-
-def classification_error(encoder, train_loader, test_loader):
-    """
-    Computes the classification error using a log-linear model (logistic regression)
-    fit to the latent representations.
-
-    Parameters:
-    encoder: Trained encoder model.
-    train_loader: Training dataloader.
-    test_loader: Testing dataloader.
-
-    Returns:
-    float: Classification error in percentage.
-    """
-    Z_train, Y_train = extract_latents(encoder, train_loader)
-    Z_test, Y_test = extract_latents(encoder, test_loader)
-
-    classifier = LogisticRegression(max_iter=1000, solver='lbfgs', multi_class='multinomial')
-    classifier.fit(Z_train, Y_train)
-
-    Y_pred = classifier.predict(Z_test)
-
-    error = 1 - accuracy_score(Y_test, Y_pred)
-
-    return error * 100  
 
 input_dim = 28 * 28
 hidden_dims = [360, 360]
