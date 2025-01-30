e5a3cd437fb52cb27bd75f9ade679674cb8dc55a
quinbez <bezawitt9@gmail.com>
2025-01-30 10:56:16 +0300

    Modified initialize weight and bias function.

--- Model Comparison/Backprop/GVAE/GVAE_model.py
+++ Model Comparison/Backprop/GVAE/GVAE_model.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class Encoder(nn.Module):
     def __init__(self, input_dim, hidden_dim, latent_dim):
         super(Encoder, self).__init__()
@@ -13,10 +12,12 @@ class Encoder(nn.Module):
 
         self._initialize_weights()
 
-    def _init_weights(self, sigma=0.1):
-        for layer in [self.fc1, self.fc2, self.fc_mu, self.fc_logvar]:
-            nn.init.normal_(layer.weight, mean=0.0, std=sigma)
-            nn.init.constant_(layer.bias, 0.0)
+    def _initialize_weights(self):
+        for m in [self.fc1, self.fc2, self.fc_mu, self.fc_logvar]:
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0.0, std=0.1)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -24,7 +25,6 @@ class Encoder(nn.Module):
         mu = self.fc_mu(x)
         logvar = self.fc_logvar(x)
         return mu, logvar
-
 
 class Decoder(nn.Module):
     def __init__(self, latent_dim, hidden_dim, input_dim):
@@ -35,17 +35,18 @@ class Decoder(nn.Module):
 
         self._initialize_weights()
 
-    def _init_weights(self, sigma=0.1):
-        for layer in [self.fc1, self.fc2, self.fc3]:
-            nn.init.normal_(layer.weight, mean=0.0, std=sigma)
-            nn.init.constant_(layer.bias, 0.0)
+    def _initialize_weights(self):
+        for m in[self.fc1, self.fc2, self.fc3]:
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0.0, std=0.1)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
 
     def forward(self, z):
         z = F.relu(self.fc1(z))
         z = F.relu(self.fc2(z))
         z = torch.sigmoid(self.fc3(z))
         return z
-
 
 class VAE(nn.Module):
     def __init__(self, input_dim, hidden_dim, latent_dim, l2_lambda):
