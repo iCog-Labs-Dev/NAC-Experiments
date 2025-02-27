d1f47ae17e26940125213a4435ba9ed21bbf2b3b
quinbez <bezawitt9@gmail.com>
2025-02-27 15:21:29 +0300

    Remove unwanted models.

--- Model_Comparison/Latest_Version/diehl_cook_snn/sim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-################################################################################
-# Simulate the DC-SNN on the MNIST database
-################################################################################
-DATA_DIR="../../data/mnist"
-
-rm -r exp/* ## clear out experimental directory
-python train_dcsnn.py --dataX="$DATA_DIR/trainX.npy" --n_samples=10000 \
-                      --n_iter=1 --verbosity=0
